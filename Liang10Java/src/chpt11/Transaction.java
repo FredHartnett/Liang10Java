3e73cca26e91f30760dd95c136d297e04fa7177b
Fred Hartnett <fred.hartnett@gmail.com>
2019-12-01 10:11:06 -0700

    commit 1 on dec 1

---
+++ Liang10Java/src/chpt11/Transaction.java
@@ -0,0 +1,42 @@
+package chpt11;
+
+public class Transaction {
+    private java.util.Date date;
+    private char type;
+    private double amount;
+    private double balance;
+    private String description;
+
+    public Transaction(char type, double amount, double balance, String description) {
+        date = new java.util.Date();
+        this.type = type;
+        this.amount = amount;
+        this.balance = balance;
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date.toString();
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+
+
+
+}
