e52f4b8cf33699126a5ac32de29fb3627f74a2e8
Fred Hartnett <fred.hartnett@gmail.com>
2019-12-12 18:05:29 -0700

    commit exercise 5.1

---
+++ Liang10Java/src/chpt5/CountNums.java
@@ -0,0 +1,39 @@
+package chpt5;
+
+public class CountNums {
+	private int countOfPositiveInts = 0;
+	private int countOfNegativeInts = 0;
+	private int sumOfInts = 0;
+	
+	public void processNum(int num) {
+		sumOfInts += num;
+		
+		if(num < 0)
+			countOfNegativeInts++;
+		else
+			countOfPositiveInts++;		
+	}
+	
+	public int getCountOfPositiveInts() {
+		return countOfPositiveInts;
+	}
+
+	public int getCountOfNegativeInts() {
+		return countOfNegativeInts;
+	}
+
+	public int getSumOfInts() {
+		return sumOfInts;
+	}
+	
+	public double getAverage() {
+		return getSumOfInts() / (double)(getCountOfPositiveInts() + getCountOfNegativeInts());
+	}
+	
+	public String output() {
+		return 	"The number of positives is " + Integer.toString(getCountOfPositiveInts()) + "\n" +
+				"The number of negatives is " + Integer.toString(getCountOfNegativeInts()) + "\n" +
+				"The total is " + Integer.toString(getSumOfInts()) + "\n" +
+				"The average is " + Double.toString(getAverage());		
+	}
+}
