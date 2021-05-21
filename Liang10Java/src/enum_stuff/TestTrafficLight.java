b048218ff635b5da93a4e4e5f30e6e13382f5cbf
FredH <fred.hartnett@gmail.com>
2021-05-21 10:24:11 -0600

    Deleted unneeded items.

--- Liang10Java/src/enum_stuff/TestTrafficLight.java
+++
@@ -1,8 +0,0 @@
-package enum_stuff;
-
-public class TestTrafficLight {
-    public static void main(String[] args) {
-        TrafficLight light = TrafficLight.RED;
-        System.out.println(light.getDescription());
-    }
-}
