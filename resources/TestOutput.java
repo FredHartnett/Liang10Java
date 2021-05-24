753c4e00cccabcb4e93681fcdd57637616bc383e
FredH <fred.hartnett@gmail.com>
2021-05-24 15:03:57 -0600

    completed exercise 12.12

---
+++ resources/TestOutput.java
@@ -0,0 +1,13 @@
+//test text file used for exercise 12.12 page 489
+
+public class TestInput {
+	public static void main(String[] args) {
+		if(1 == 1) {
+			System.out.println("first if statement");
+		}
+		
+		if(2 == 2) {
+			System.out.println("second if statement");
+		}			
+	}	
+}
--- src/chpt12/Ex12_12.java
+++ src/chpt12/Ex12_12.java
@@ -1,19 +1,54 @@
 package chpt12;
 
 import java.io.File;
-
-//Ex. 12.12 p.489
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ex12_12 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        String inputFile = "resources/TestInput.java";
+        String outputFile = "resources/TestOutput.java";
+        File targetFile = new File(outputFile);
+        ArrayList<String> oldFormat = new ArrayList<>();
+        ArrayList<String> newFormat = new ArrayList<>();
 
-        String file = "resources/Test.java";
-
-        // Check if source file exists
-        File sourceFile = new File(file);
-        if (!sourceFile.exists()) {
+        //Check if source file exists
+        File sourceFile = new File(inputFile);
+        if(!sourceFile.exists()) {
             System.out.println("*** source file does not exist ***");
             System.exit(1);
         }
+
+        Scanner input = new Scanner(sourceFile);
+
+        PrintWriter output = new PrintWriter(targetFile);
+
+        while (input.hasNext()) {
+            oldFormat.add(input.nextLine());
+        }
+
+        newFormat = reformatTheBraces(oldFormat);
+
+        for (String str : newFormat) {
+            output.println(str);
+        }
+
+        output.close();
+    }
+
+    private static ArrayList<String> reformatTheBraces(ArrayList<String> reformatBracesList) {
+
+        for (int i = 0; i < reformatBracesList.size(); i++) {
+            String tmp = reformatBracesList.get(i);
+            tmp = tmp.trim();
+
+            if(tmp.equals("{")) {
+                reformatBracesList.set(i-1,reformatBracesList.get(i-1) + " {");
+                reformatBracesList.remove(i);
+            }
+        }
+        return reformatBracesList;
     }
 }
